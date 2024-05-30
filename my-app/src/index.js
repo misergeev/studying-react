3c31430ea6c91561063958f341cffabf35124d2e
Michail <mialsergeev@gmail.com>
2024-05-30 23:21:23 +0500

    About JSX for start

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const text = 'Hello World!';
+
+const elem = (
+  <div>
+    <h2 className=''>Текст: {text}</h2>
+    <input type="text" id="name"/>
+    <label htmlFor="name"></label>
+    <button tabIndex="0">Click</button>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  elem
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
